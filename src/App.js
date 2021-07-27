7a0b24c19b472b0c114791d2ec9d6fd23d40355e
imoempire <mrimoempire@gmail.com>
2021-07-27 12:17:46 +0000

    add route and link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter, Link, Route} from "react-router-dom"
 import './App.css';
+import Home from "./Components/Home";
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Services from './Components/Services';
+import ServicesDetail from "./Components/ServicesDetail";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Link to="/">Home</Link>
+    <Link to="/About">About</Link>
+    <Link to="/Services">Services</Link>
+    <Link to="/Contact">Contact</Link>
     </div>
+
+    <Route path="/" exact component={Home} />
+    <Route path="/About" exact component={About} />
+    <Route path="/Services" exact component={Services} />
+    <Route path="/ServicesDetail/:id" exact component={ServicesDetail} />
+    <Route path="/Contact" exact component={Contact} />
+
+    </BrowserRouter>
   );
 }
 
